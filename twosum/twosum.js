1d7c75420df383e748232970923778796f2d7ffb
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-06 14:31:41 -0700

    setting up ioce for twosum

--- twosum/twosum.js
+++ twosum/twosum.js
@@ -1,10 +1,25 @@
 /**
+ * 
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ * You can return the answer in any order.
+ * 
  * @param {number[]} nums
  * @param {number} target
  * @return {number[]}
+ * 
+ * E.G
+ * input: nums = [2,7,11,15], target = 9
+ * output: [0,1]
  */
- var twoSum = function(nums, target) {
-    
+var twoSum = function(nums, target) {
+
+  // input: array of numbers, num / integer target, target
+  // output: an array that stores the indices of the two numbers that add up to target
+  // constraint: exactly one solution in array
+  //        - may not use the same element twice
+  // edge case: if array nums has only one number
+
 };
 
 module.exports = twoSum;
