91fb3f02709a9b4c3f08c51ec84eac7d851ae34f
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-06 15:50:34 -0700

    max profit problem solved

--- buyStock/buyStock.js
+++ buyStock/buyStock.js
@@ -3,12 +3,50 @@
  * You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
  * Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
  * 
+ * E.G.
+ * prices = [7,1,5,3,6,4]
+ * output: 5
+ * 
+ * prices = [7,6,4,3,1]
+ * output: 0 bc/ it always decreases
+ * 
  * @param {number[]} prices
  * @return {number}
  * 
  */
 var maxProfit = function(prices) {
-    
+
+  // input: an array of prices
+  //   prices[i] = price of stock
+  //   i = day 
+  // output: the maximum profit 
+  // constraints: you can only sell after you buy (so selling day has to come after buying day)
+  // edge case: there is no greater value after buying stock
+  // if you cannot achieve profit, return 0
+
+  // iterate through the prices array
+  // grab each element and then the max value of the remaining array
+  // if element is > max value, then return 0
+  // else return max value - element
+
+  let buyDay = 0;
+  let sellDay = 1;
+
+  let maxProfit = 0;
+
+  while (sellDay < prices.length) {
+    // check whether it's profitable
+    if (prices[sellDay] > prices[buyDay]) {
+      maxProfit = maxProfit > prices[sellDay] - prices[buyDay] ? maxProfit : prices[sellDay] - prices[buyDay]
+    } else {
+      buyDay = sellDay;
+    }
+
+    sellDay++;
+  }
+
+  return maxProfit;
+
 };
 
 module.exports = maxProfit;
--- buyStock/buyStock.test.js
+++ buyStock/buyStock.test.js
@@ -1,7 +1,16 @@
+
 const maxProfit = require('./buyStock');
 
 describe('Max Profit', () => {
   it('Should be a function', () => {
     expect(typeof maxProfit).toBe('function');
+  })
+
+  it('Example 1', () => {
+    expect(maxProfit([7, 1, 5, 3, 6, 4])).toBe(5);
+  })
+
+  it('Example 2', () => {
+    expect(maxProfit([7, 6, 4, 3, 1])).toBe(0);
   })
 })
