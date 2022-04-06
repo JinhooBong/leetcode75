8e9b925ab0e42058cf67b65f3f32a559cc63cd5c
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-06 14:53:08 -0700

    solution for twosum

--- twosum/twosum.js
+++ twosum/twosum.js
@@ -20,6 +20,29 @@ var twoSum = function(nums, target) {
   //        - may not use the same element twice
   // edge case: if array nums has only one number
 
+  // create an array variable to store the indices of the two numbers
+  // for (let i = 0; i < nums.length - 1; i++) {
+  //    for (let j = i + 1; j < nums.length; j++) {}
+
+  // if any element is greater than the target, then we don't have to worry about testing it
+
+  // if i + j === target, then we push i & j into our array variable
+
+  // return array variable
+
+  let indiceArr = [];
+
+  for (let i = 0; i < nums.length - 1; i++) {
+      for (let j = i + 1; j < nums.length; j++) {
+          if (nums[i] + nums[j] === target) {
+            indiceArr.push(i);
+            indiceArr.push(j);
+          }
+        }
+  }
+
+  return indiceArr;
+
 };
 
 module.exports = twoSum;
--- twosum/twosum.test.js
+++ twosum/twosum.test.js
@@ -1,7 +1,24 @@
+
 const twoSum = require('./twosum');
 
 describe('TwoSum', () => {
   it('Should be a function', () => {
     expect(typeof twoSum).toBe('function');
+  });
+
+  it('Example 1', () => {
+    expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+  });
+
+  it('Example 2', () => {
+    expect(twoSum([3, 2, 4], 6)).toStrictEqual([1, 2]);
   })
-})
+
+  it('Example 3', () => {
+    expect(twoSum([3, 3], 6)).toStrictEqual([0, 1]);
+  })
+
+  it('Example 4', () => {
+    expect(twoSum([-3, 4, 3, 90], 0)).toStrictEqual([0, 2])
+  })
+});
