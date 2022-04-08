e6cd820d9e9927005d4467c59b61ef9b4c50e57b
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-07 22:51:21 -0700

    longest substring without repeating characters problem set up

---
+++ substringWithoutRepeat/maxNoRepeat.js
@@ -0,0 +1,11 @@
+/**
+ * Given a string s, find the length of the longest substring without repeating characters.
+ * 
+ * 
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function(s) {
+};
+
+module.exports = lengthOfLongestSubstring;
---
+++ substringWithoutRepeat/maxNoRepeat.test.js
@@ -0,0 +1,22 @@
+s = "abcabcbb" = "bbbbb""pwwkew"
+const maxNoRepeat = require('./maxNoRepeat');
+
+describe('Longest Substring without Repeating Characters', () => {
+
+  it('Should be a function', () => {
+    expect(typeof maxNoRepeat).toBe('function');
+  })
+
+  it('Example 1', () => {
+    expect(maxNoRepeat('abcabcbb')).toBe(3);
+  })
+
+  it('Example 2', () => {
+    expect(maxNoRepeat('bbbb')).toBe(1);
+  })
+
+  it('Example 3', () => {
+    expect(maxNoRepeat('pwwkew')).toBe(3);
+  })
+
+})
