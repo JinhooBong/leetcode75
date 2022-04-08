889aee80c545ac8c586f666a0e5987e71d11f3c5
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-07 23:47:12 -0700

    longest palindromic substring solution

--- longestPalindrome/longestPalindrome.js
+++ longestPalindrome/longestPalindrome.js
@@ -20,12 +20,32 @@ var longestPalindrome = function(s) {
     for (let i = 0; i < s.length; i++) {
       // if s is odd
       if (s.length % 2 !== 0) {
-
+        let l = i;
+        let r = i;
+        while ((l >= 0 && r < s.length) && s[l] === s[r]) {
+          if ((r - l + 1) > resultLength) {
+            result = s.substring(l, r + 1);
+            resultLength = r - l + 1;
+          }
+          l -= 1;
+          r += 1;
+        }
       } else {
         // if s is even
+        let l = i;
+        let r = i + 1;
+        while ((l >= 0 && r < s.length) && s[l] === s[r]) {
+          if ((r - l + 1) > resultLength) {
+            result = s.substring(l, r + 1);
+            resultLength = r - l + 1;
+          }
+          l -= 1;
+          r += 1;
+        }
       }
-
     }
+
+    return result;
 };
 
 module.exports = longestPalindrome;
--- longestPalindrome/longestPalindrome.test.js
+++ longestPalindrome/longestPalindrome.test.js
@@ -1,10 +1,17 @@
-
 const longestPalindrome = require('./longestPalindrome');
 
 describe('Longest Palindromic Substring', () => {
 
   it('Should be a function', () => {
     expect(typeof longestPalindrome).toBe('function');
+  })
+
+  it('Example 1', () => {
+    expect(longestPalindrome("babad")).toStrictEqual("bab");
+  })
+
+  it('Example 2', () => {
+    expect(longestPalindrome("cbbd")).toBe("bb");
   })
 
 })
