ceae260b497aa7977765b359a64c67f18a68618f
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-08 22:57:47 -0700

    twosum II solution

--- twoSumII/twoSumII.js
+++ twoSumII/twoSumII.js
@@ -11,6 +11,30 @@
   // cannot use the same element twice
   
 
+  // two pointer problem
+  // left pointer at beginning,
+  // right pointer at end
+  // this is a sorted array so take advantage
+
+  // if the sum > target, we can decrease the right pointer
+  // if sum < target, we can increase left pointer
+  // if sum === target, we return the indices + 1
+
+  let l = 0; 
+  let r = numbers.length - 1;
+
+  while (l < r) {
+    let sum = numbers[l] + numbers[r];
+
+    if (sum > target) {
+      r--;
+    } else if (sum < target) {
+      l++;
+    } else {
+      return [l + 1, r + 1];
+    }
+  }
+
 };
 
 module.exports = twoSum;
