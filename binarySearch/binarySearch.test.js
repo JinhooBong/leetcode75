7e010f45990d74280a0957ad630fe232ff52186a
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-09 11:07:35 -0700

    binary search problem set up

---
+++ binarySearch/binarySearch.test.js
@@ -0,0 +1,9 @@
+const binarySearch = require('./binarySearch');
+
+describe('Binary Search', () => {
+
+  it('Should be a function', () => {
+    expect(typeof binarySearch).toBe('function');
+  })
+
+})
