6779198fb726f63ed5be866619c176771d8c86f9
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-09 11:11:11 -0700

    binary search problem solution

--- binarySearch/binarySearch.js
+++ binarySearch/binarySearch.js
@@ -4,6 +4,36 @@
  * @return {number}
  */
  var search = function(nums, target) {
+
+  // input: an array of integers in sorted order, and a target value
+  // output: the index at which the target exists, else - 1
+
+  // constraints: must be solved in O(logn) time
+
+  // we have two pointers l = 0, r = nums.length - 1
+  // while our left pointer hasn't overlapped, 
+  // we grab the middle index
+  // if target > nums[middle], then we shift our left pointer to mid + 1
+  // if target < nums[middle], then we shift our right pointer to mid - 1;
+  // else we return the middle index bc we found our target
+  // if our whole loop runs through and doesn't return, we return -1
+
+  let l = 0;
+  let r = nums.length - 1;
+
+  while (l < r) {
+    let mid = Math.floor((l + r) / 2);
+
+    if (target > nums[mid]) {
+      l = mid + 1;
+    } else if (target < nums[mid]) {
+      r = mid - 1;
+    } else {
+      return mid;
+    }
+  }
+
+  return -1;
   
 };
 
--- binarySearch/binarySearch.test.js
+++ binarySearch/binarySearch.test.js
@@ -6,4 +6,12 @@ describe('Binary Search', () => {
     expect(typeof binarySearch).toBe('function');
   })
 
+  it('Example 1', () => {
+    expect(binarySearch([-1,0,3,5,9,12], 9)).toBe(4);
+  })
+
+  it('Example 2', () => {
+    expect(binarySearch([-1,0,3,5,9,12], 2)).toBe(-1);
+  })
+
 })
