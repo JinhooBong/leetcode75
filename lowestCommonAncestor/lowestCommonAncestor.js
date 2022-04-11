229b04a32160d413600b2cffa1f9eccabb99dbe4
Jinhoo Bong <jinhoobong@gmail.com>
2022-04-11 16:52:53 -0700

    lowest common ancestor problem set up and solution

---
+++ lowestCommonAncestor/lowestCommonAncestor.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+ var lowestCommonAncestor = function(root, p, q) {
+  // input: root value, p and q values
+  // output: the lowest common ancestory
+  // edge case: a node can be the descendant of itself
+
+  // while we have a root node
+  while (root) {
+    if (p.val < root.val && q.val < root.val) {
+      root = root.left;
+    } else if (p.val > root.val && q.val > root.val) {
+      root = root.right;
+    } else {
+      return root;
+    }
+  }
+  
+  
+};
